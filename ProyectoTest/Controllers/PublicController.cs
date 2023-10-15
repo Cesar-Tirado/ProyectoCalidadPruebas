c0c599e0d61a701e7ea58f1da0444e10b1bf9e30
Cesar-Tirado <cesar.tiradocent@gmail.com>
2023-10-14 22:59:57 -0500

    primera Subida

---
+++ ProyectoTest/Controllers/PublicController.cs
@@ -0,0 +1,29 @@
+﻿using ProyectoTest.Logica;
+using ProyectoTest.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ProyectoTest.Controllers
+{
+    public class PublicController : Controller
+    {
+        private static Usuario oUsuario;
+        //VISTA
+        public ActionResult Index()
+        {
+            oUsuario = new Usuario();
+            oUsuario.Correo = "cesar@admin.com";
+            oUsuario.IdUsuario = 2;
+            oUsuario.Nombres = "cesar";
+            Session["Usuario"] = oUsuario;
+            return View();
+        }
+
+    }
+
+}
---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        //public static string CN = "Data Source=VMI266939; Initial Catalog=LONGHORN_ADMIN; User ID=user; Password=usuario;";
+        //public static string CN = "Data Source=DESKTOP-D7VGCJ9; Initial Catalog=LONGHORN_ADMIN2; User ID=user1; Password=usuario;";
+        public static string CN = "Data Source=localhost;Initial Catalog=LONGHORN_ADMIN;Integrated Security=True;User ID =cesar;Password=cesar";
+        //public static string CN = "Data Source=DESKTOP-D7VGCJ9;Initial Catalog=LONGHORN_ADMIN;Integrated Security=True"; // LocalAngel
+    }
+}
